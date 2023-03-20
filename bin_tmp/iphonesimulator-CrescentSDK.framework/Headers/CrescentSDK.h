aca6adb84f6d8fe9ad8fcb3f27419c833fc00390
cyh <cyh@gopluslabs.io>
2023-03-20 18:24:02 +0800

    generate creascent.framework

---
+++ bin_tmp/iphonesimulator-CrescentSDK.framework/Headers/CrescentSDK.h
@@ -0,0 +1,18 @@
+//
+//  CrescentSDK.h
+//  CrescentSDK
+//
+//  Created by cyh on 2023/2/7.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CrescentSDK.
+FOUNDATION_EXPORT double CrescentSDKVersionNumber;
+
+//! Project version string for CrescentSDK.
+FOUNDATION_EXPORT const unsigned char CrescentSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrescentSDK/PublicHeader.h>
+
+
